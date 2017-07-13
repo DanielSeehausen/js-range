43cc5041232397eaf52bec33c8ac38765c0a624b
Daniel Seehausen <Daniel.Seehausen@gmail.com>
2017-07-13 11:09:35 -0400

    finished initial xrange -- starting testing now. currently a mixture of list init plans and range, so going to need to split those up asap

--- xrange.js
+++ xrange.js
@@ -1,19 +1,78 @@
+// TODO wrap privates in closure -- find out whats best option for that $peed
+
 if (typeof xrange === 'function')
   throw "'xrange' function already defined elsewhere -- there can only be one Christopher Lambert!"
 
-function xrange(start, stop, step=1, operator="+") {
+// Private
+// *****************************************************************************
+const operators = new Set(['+', 'log', 'pow'])
 
-  if (arguments.length === 1)
-    return xrange(0, start)
-
-  if (isNaN(parseFloat(start)) || isNaN(parseFloat(stop)) || isNaN(parseFloat(step)) || !(new Set(['+', 'log', 'pow']).has(operator))
-    throw "'invalid arguments passed to xrange -- format parameters should follow the following format: (start<int>, stop<int>, range<int=1>, operator<one of ['+', 'pow', 'log']>)'"
-
-  if (step === 0 || start === stop || (start > stop && step > 0) || (start < stop && step < 0))
-    throw `invalid arguments provided: start - ${start}, stop - ${stop}, step - ${step}`
-
-
-
+function assertTypes(start, stop, step) {
+  for (var arg in arguments) {
+    if (isNaN(parseFloat(arguments[arg]))
+      throw `'invalid arguments passed to xrange -- parameters must match the following: (start<int>, stop<int>, range<int=1>'\nReceived: (${start}, ${stop}, ${step})`
+  }
 }
 
-// Array.prototype
+function assertValidOperator(operator) {
+  if (!operators.has(operator))
+    throw `invalid operator passed to xrange -- operator should be one of: ['+', '*', '/', 'pow', 'log']\nReceived: ${operator}`
+}
+
+function assertValues(start, stop, step) {
+  if (step === 0 || start === stop || (start > stop && step > 0) || (start < stop && step < 0))
+    throw `unresolveable values passed to xrange -- received: (start<${start}>, stop<${stop}>, range<${step}>)`
+}
+
+function assertValid(start, stop, step, operator) {
+  assertTypes(start, stop, step)
+  assertOperator(operator)
+  assertValues(start, stop, step)
+}
+
+const dispatch = {
+    "+" : (op1, op2) =>  {
+        return op1 + op2
+    },
+    "*" : (op1, op2) =>  {
+      return op1 * op2
+    },
+    "/" : (op1, op2) =>  {
+        return op1 / op2
+    },
+    "pow" : (op1, op2) => {
+        return Math.pow(op1, op2)
+    },
+    "log" : (op1) => {
+        return Math.log(op1)
+    }
+}
+
+
+function genRangeArr(start, stop, step, operator) {
+  let arr = new Array(Math.ceil(Math.abs(start - stop))/step)
+  // TODO better to multiply step by iteration? Probably not. need to benchmark. not high priority
+  arr[0] = start
+  for (let idx = 1; idx < arr.length; idx++) {
+    arr[idx] = dispatch[operator](arr[idx-1], step)
+  }
+  return arr
+}
+// *****************************************************************************
+
+
+function xrange(start, stop, step=1, operator="+") {
+  // [start, stop)
+
+  // TODO high-priority: the */\//pow/log should really be put into a list initialization function instead of here (because the range really represents the iterations and no longer the integer range...)
+
+  if (arguments.length === 1) {
+    stop = start
+    start = 0
+  }
+
+  assertValid(start, stop, step, operator)
+  return (start > stop) ? genRangeArr(stop, start, step, operator) : genRangeArr(start, stop, step, operator)
+}
+
+console.log(xrange(10))
